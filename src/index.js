a8e9b820808024fa26c886ef43c93c44b22a8632
DevMasteryLabs <develop.devmastery@gmail.com>
2022-09-11 15:45:25 +0100

    Set up bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
