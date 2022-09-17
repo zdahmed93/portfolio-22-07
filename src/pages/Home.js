7f602541da47227a1226eb41d2841396b0a09052
DevMasteryLabs <develop.devmastery@gmail.com>
2022-09-17 17:18:25 +0100

    Add the Projects page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import webImage from '../images/web.png'
 
 function Home() {
   return (
-    <div>
+    <div className='alert alert-primary'>
       <img src="/images/my-photo.jpg" alt="My Photo" className='avatar' />
       <h1>Hello My name is Rayen. I am a Fullstack Web Developer</h1>
       <img src={webImage} alt="Web Development" />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+const myProjects = [
+  {
+    title: 'Business Land',
+    description: 'A simple landing page developed with HTML and CSS',
+    url: 'https://business-land.vercel.app/'
+  },
+  {
+    title: 'TODODM',
+    description: 'A simple todo list developed with HTML, CSS and Javascript',
+    url: 'https://todom-devmastery.vercel.app/'
+  }
+]
+
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      {myProjects.map((item, index) => (
+        <div style={{ padding: 20 }} >
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{item.title}</h5>
+              <p class="card-text">{item.description}</p>
+              <a href={item.url} target='_blank' class="card-link">Preview</a>
+            </div>
+          </div>
+        </div>
+      ))}      
+    </div>
   )
 }
 
