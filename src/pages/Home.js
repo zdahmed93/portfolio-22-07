809379a0d169855c3b986350d00368f47487d220
DevMasteryLabs <develop.devmastery@gmail.com>
2022-09-17 15:49:40 +0100

    Add the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import webImage from '../images/web.png'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <img src="/images/my-photo.jpg" alt="My Photo" className='avatar' />
+      <h1>Hello My name is Rayen. I am a Fullstack Web Developer</h1>
+      <img src={webImage} alt="Web Development" />
+    </div>
   )
 }
 
