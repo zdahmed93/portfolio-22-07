c65508ed10c5352a44ba6d1fd3ea32ce23332993
DevMasteryLabs <develop.devmastery@gmail.com>
2022-09-18 14:57:29 +0100

    Fix the card content

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -15,9 +15,8 @@ function Skills() {
           <div class="card">
             <img src={item.image} class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">{item.name}</h5>
+                <a href="#" class="btn btn-primary">Learn about</a>
               </div>
           </div>
         </div>
